ac7566786d3adfc4d74270cb8da2a6c68a614855
Robert S <roberts@eostar.com>
2018-01-07 00:42:14 -0500

    Add project files.

---
+++ Colorum/Colorum/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Colorum.Models;
+
+namespace Colorum.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
